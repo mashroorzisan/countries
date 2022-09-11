dd760810bd9d36476ed19f3cdd3e5b07ea7f4eea
mashroorzisan <mashroorzisan@gmail.com>
2022-09-11 13:40:59 +0600

    added some section

--- js/api.js
+++ js/api.js
@@ -1,5 +1,19 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
-        .then(data => console.log(data))
-}
+        .then(data => getFunction(data))
+};
+
+const getFunction = (countries) => {
+    const countrySection = document.getElementById('countries-section');
+    countries.forEach(country => {
+        // console.log(country.name.common)
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('style-country');
+        countryDiv.innerHTML = `
+        <h1>Name: ${country.name.common} </h1>
+        <button>details</button>
+        `
+        countrySection.appendChild(countryDiv);
+    });
+};
